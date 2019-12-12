556b96b291653aaab45ade3df4b1a693604ed0f7
MaryAnn Chukwuka <annychuks07@gmail.com>
2019-12-12 16:41:13 +0100

    add authenticity_token to HTML form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
-  resources :users, only: [:new, :create]
+  # resources :users, only: [:new, :create, :edit, :update]
+  resources :users, except: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
